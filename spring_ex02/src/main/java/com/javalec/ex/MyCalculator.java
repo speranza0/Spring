390916e5b85bbb585095e6473ab8ab6d5fe76d90
user <user@DESKTOP-P1K3VG9>
2022-06-06 16:56:24 +0900

    Spring 4강 DI(Dependency Injection)-2까지

---
+++ spring_ex02/src/main/java/com/javalec/ex/MyCalculator.java
@@ -0,0 +1,42 @@
+package com.javalec.ex;
+
+public class MyCalculator {
+	Calculator calculator;
+	private int firstNum;
+	private  int secondNum;
+	
+	public Calculator getCalculator() {
+		return calculator;
+	}
+	public void setCalculator(Calculator calculator) {
+		this.calculator = calculator;
+	}
+	public int getFirstNum() {
+		return firstNum;
+	}
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+	public int getSecondNum() {
+		return secondNum;
+	}
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+	
+	public void add() {
+		calculator.addition(firstNum, secondNum);
+	}
+	
+	public void sub() {
+		calculator.subtraction(firstNum, secondNum);
+	}
+	
+	public void multi() {
+		calculator.multiplication(firstNum, secondNum);
+	}
+	
+	public void div() {
+		calculator.division(firstNum, secondNum);
+	}
+}
