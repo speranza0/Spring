e7d3126809c8a164af0508c6e714355b0d5c3105
user <user@DESKTOP-P1K3VG9>
2022-06-09 18:56:14 +0900

    Spring 15강 폼 데이터 값 검증

---
+++ spring_ex15/src/main/java/com/javalec/ex/StudentController.java
@@ -0,0 +1,35 @@
+package com.javalec.ex;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+	@RequestMapping("/studentForm")
+	public String studentForm() {
+		return "createPage";
+	}
+	
+	@RequestMapping("/student/create")
+	public String studentCreate(@ModelAttribute("student") @Valid Student student, BindingResult result) {
+		String page = "createDonePage";
+		
+//		StudentValidator validator = new StudentValidator();
+//		validator.validate(student, result);
+		if(result.hasErrors()) {
+			page = "createPage";
+		}
+		return page;
+	}
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new StudentValidator());
+	}
+}
